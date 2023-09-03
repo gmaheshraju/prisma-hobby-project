6dc74830e59b4a8353d38f58d5f371c833ceb3ab
Mahesh Guntumadugu <g.maheshlearn@outlook.com>
2023-09-03 20:35:20 +0530

    update

--- src/01-create-user.ts
+++ src/01-create-user.ts
@@ -6,8 +6,8 @@ const prisma = new PrismaClient()
 async function main() {
   await prisma.user.create({
     data: {
-     email: "harkirat1@gmail.com",
-     name: "harkriat"
+     email: "mahesh@gmail.com",
+     name: "mahesh"
     }
   })
 }
